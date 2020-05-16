d65c1e21752caa5f4489fd55040eb880888bd91c
Luis Ruiz Pavón <luis_ruiz_pavon@hotmail.com>
2020-05-16 10:08:59 +0200

    Update README

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Add the dotnet path to the path
+export PATH="$HOME/.dotnet":$PATH
+
+if [ -d "./artifacts" ]
+then
+    rm -Rf "./artifacts"; 
+fi
+
+dotnet restore
+
+commitHash=$(git rev-parse --short HEAD)
+suffix="-ci-local"
+buildSuffix="$suffix-$commitHash"
+
+echo "build: Version suffix is $buildSuffix"
+
+dotnet build Notes.sln -c Release --version-suffix "$buildSuffix"  -v q /nologo
